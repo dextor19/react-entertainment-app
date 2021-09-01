995ae402e573c17bbe7963c0076cfbcf4de8851a
Dexter Griffiths <dexgriffiths@hotmail.co.uk>
2021-09-01 15:32:36 +0900

    create scss modules correctly

--- src/components/common/App/index.tsx
+++ src/components/common/App/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import './App.scss';
+import logo from './logo.svg';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
